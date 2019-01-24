dbaf30f94155cb44563cc54f9282d0e016b12eba
Amy Huang <yh574@cornell.edu>
2019-01-24 17:42:51 -0500

    updates

--- scripts/feature.js
+++ scripts/feature.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
-  var images = [
-    "images/tidyingup.jpb",
-  ];
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-  var currentIndex = 0;
-
-  $("#slideNext").click(function(){
-    if (currentIndex < 1){
-      currentIndex = currentIndex + 1;
-       document.getElementById("featured").style.backgroundImage = "url(images[currentIndex])";
-    }
+  $("#img-clck").click(function(){
+    showDivs(slideIndex += 1);
   });
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("feature");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }
 
 });
